94d1141a22449d63b98be441ea7c13677727d6b8
interrrp <int@addictedto.coffee>
2024-04-27 19:41:07 +0800

    Color logs

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/interrrp/pinned
 
 go 1.22.2
 
-require github.com/stretchr/testify v1.9.0
+require (
+	github.com/lmittmann/tint v1.0.4
+	github.com/stretchr/testify v1.9.0
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
--- main.go
+++ main.go
@@ -3,14 +3,18 @@ package main
 import (
 	"log/slog"
 	"net/http"
+	"os"
 
 	"github.com/interrrp/pinned/pin"
 	"github.com/interrrp/pinned/server"
+	"github.com/lmittmann/tint"
 )
 
 const Addr = ":8080"
 
 func main() {
+	slog.SetDefault(slog.New(tint.NewHandler(os.Stderr, nil)))
+
 	pinSvc := pin.NewInMemoryPINService()
 	s := server.NewServer(pinSvc)
 
