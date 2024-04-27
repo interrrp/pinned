5a1bc895f91904638f5f69e20af8802d96f5d327
interrrp <int@addictedto.coffee>
2024-04-27 16:15:01 +0800

    Add guessing route

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io"
 	"net/http"
 
 	"github.com/interrrp/pinned/pin"
@@ -17,9 +18,34 @@ func NewServer(pinSvc pin.PINService) *Server {
 		mux:    http.NewServeMux(),
 	}
 
+	s.mux.HandleFunc("POST /", s.handleGuess)
+
 	return s
+}
+
+func (s *Server) handleGuess(w http.ResponseWriter, r *http.Request) {
+	guess, err := readBodyToString(r)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if s.pinSvc.IsCorrect(guess) {
+		w.WriteHeader(http.StatusOK)
+		s.pinSvc.Generate()
+	} else {
+		w.WriteHeader(http.StatusForbidden)
+	}
 }
 
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	s.mux.ServeHTTP(w, r)
+}
+
+func readBodyToString(r *http.Request) (string, error) {
+	b, err := io.ReadAll(r.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(b), nil
 }
