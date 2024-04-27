9c8670fc2f82cde74a6bac495aaca59c2198276e
interrrp <int@addictedto.coffee>
2024-04-27 16:48:04 +0800

    Add tests for guess endpoint

---
+++ server/guess_test.go
@@ -0,0 +1,42 @@
+package server
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/interrrp/pinned/pin"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestValidGuess(t *testing.T) {
+	pinSvc := pin.NewInMemoryPINService()
+	s := NewServer(pinSvc)
+
+	req := newGuessRequest(pinSvc.CurrentPIN())
+	res := httptest.NewRecorder()
+	s.ServeHTTP(res, req)
+
+	assert.Equal(t, http.StatusOK, res.Result().StatusCode)
+}
+
+func TestInvalidGuess(t *testing.T) {
+	pinSvc := pin.NewInMemoryPINService()
+	s := NewServer(pinSvc)
+
+	req := newGuessRequest("Some invalid PIN")
+	res := httptest.NewRecorder()
+	s.ServeHTTP(res, req)
+
+	assert.Equal(t, http.StatusForbidden, res.Result().StatusCode)
+}
+
+func newGuessRequest(pin string) *http.Request {
+	req, _ := http.NewRequest(
+		http.MethodPost,
+		"/",
+		strings.NewReader(pin),
+	)
+	return req
+}
